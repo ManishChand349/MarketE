b7457c25be374bfc3c28792af9c2a3a191b0e280
Manish Chand <manishchandr349@gmail.com>
2023-07-25 22:34:52 +0530

    first

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import logoDark from "./logoDark.png"
+import logoLight from "./logoLight.png"
+import cart from "./cart.png"
+import paymentLogo from "./paymentLogo.png"
+import githubLogo from "./githubLogo.png"
+import googleLogo from "./googleLogo.png"
+
+export {logoDark, logoLight,cart,paymentLogo,githubLogo,googleLogo};
