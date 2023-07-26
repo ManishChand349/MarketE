602dabb4dfc264e8acfa298598fdff7a23717fa9
Manish Chand <manishchandr349@gmail.com>
2023-07-26 18:56:31 +0530

    adding addtocart

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+import bazarRedcure from "./bazarSlice";
+
+export const store = configureStore({
+  reducer: {
+     bazar: bazarRedcure,
+  },
+})
+
